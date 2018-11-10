390c837b7c60990736a8cb065144c6cd28e92238
pcraig3 <paul_craig_16@hotmail.com>
2018-11-10 01:25:46 -0500

    develop on `dev` branch, release on `master` branch

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -10,7 +10,7 @@ fi
 npm run be # build and export static html files
 git add .
 git commit -m "build and export static html files"
-git subtree split --prefix out -b gh-pages # create a local gh-pages branch containing the splitted output folder
-git push -f origin gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
-git branch -D gh-pages # delete the local gh-pages because you will need it: ref
-# git reset --hard HEAD~1 # remove the commit where the static html was exported. we don't need this in the history
+git subtree split --prefix out -b master # create a local master branch containing the splitted output folder
+git push -f origin master:master # force the push of the master branch to the remote gh-pages branch at origin
+git branch -D master # delete the local master because you don't need it
+git reset --hard HEAD~1 # remove the commit where the static html was exported. we don't need this in the history
