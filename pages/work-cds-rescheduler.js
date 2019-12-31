3547cddb776e5574826266dddf7c16c1207afaf8
Paul Craig <paul_craig_16@hotmail.com>
2019-12-31 08:12:39 -0500

    Update the rescheduler post because it died

--- pages/work-cds-rescheduler.js
+++ pages/work-cds-rescheduler.js
@@ -3,12 +3,10 @@ import Post from '../components/post'
 export default () => (
   <Post
     h1="Reschedule a citizenship test"
-    subheading="Helping future Canadians today"
+    subheading="Helping future Canadians (yesterday)"
     title="CDS Citizenship Rescheduler — paul craig"
     imgSrc={require('../img/ircc.png')}
     imgAlt="Screenshot of the Citizenship Rescheduler"
-    linkText="The Citizenship Rescheduler"
-    href={'https://vancouver.rescheduler-dev.cds-snc.ca/'}
   >
     <p>
       From April to October 2018, I led the development of the{' '}
@@ -19,27 +17,27 @@ export default () => (
       <a href="https://digital.canada.ca/" target="_blank">
         Canadian Digital Service
       </a>
-      . The Rescheduler allows future Canadians with prior obligations to rebook
-      their citizenship language tests, replacing an anxiety-riven informal
-      process with a far more straightforward interaction. It’s better for end
-      users, and it’s better for immigration officers.
+      . The Rescheduler allowed future Canadians with prior obligations to
+      rebook their citizenship language tests, replacing an anxiety-riven
+      informal process with a far more straightforward interaction. It was
+      better for end users, and it was better for immigration officers.
     </p>
     <p>
-      It’s written as a{' '}
+      It was written as a{' '}
       <a
         href="https://medium.freecodecamp.org/demystifying-reacts-server-side-render-de335d408fe4"
         target="_blank"
       >
         server-rendered React
       </a>{' '}
-      app, it logs errors in production,{' '}
+      app, it logged errors in production,{' '}
       <a
         href="https://digital.canada.ca/2018/08/08/supporting-users-gracefully-degrading-react/"
         target="_blank"
       >
-        it supports a no-JS user flow
+        it supported a no-JS user flow
       </a>
-      , and it’s exceptionally well tested (thanks largely to{' '}
+      , and it was exceptionally well tested (thanks largely to{' '}
       <a
         href="https://twitter.com/timarney"
         target="_blank"
@@ -47,10 +45,10 @@ export default () => (
       >
         Tim Arney
       </a>
-      ). Even when it breaks, it works.
+      ). Even when it broke, it worked.
     </p>
     <p>
-      The Rescheduler is the first{' '}
+      The Rescheduler was the first{' '}
       <a
         href="https://www.tpsgc-pwgsc.gc.ca/esc-src/protection-safeguarding/niveaux-levels-eng.html"
         target="_blank"
@@ -61,8 +59,8 @@ export default () => (
       <a href="https://digital.canada.ca/our-team/" target="_blank">
         a world-class team
       </a>
-      , and every day it helps <strong>real people</strong> get on with their
-      lives.
+      , and for nearly a year it helped <strong>real people</strong> get on with
+      their lives.
     </p>
   </Post>
 )
