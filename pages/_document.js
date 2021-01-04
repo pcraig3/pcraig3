ce127d74fb18072be5e218f5dcb52bdbb821b887
Paul Craig <paul@pcraig3.ca>
2021-01-04 01:52:01 -0500

    add some font preload stuff to make the site a bit faster

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,16 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* tags in here appears after the tags in <HEAD> in Layout */}
-          <link href="https://fonts.googleapis.com/css?family=Gothic+A1:400,600" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
+          <link
+            rel="preload"
+            as="style"
+            href="https://fonts.googleapis.com/css?family=Gothic+A1:400,600&amp;display=fallback"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Gothic+A1:400,600&amp;display=fallback"
+          />
         </Head>
         <body>
           <Main />
